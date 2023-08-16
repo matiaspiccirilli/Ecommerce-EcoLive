f59abb66861a2b576503946cc77eb682d3b6ced6
matiaspiccirilli <matiaspiccirilli3@gmail.com>
2023-08-16 12:06:44 -0300

    Mensaje descriptivo del commit

---
+++ frontend/src/App.js
@@ -0,0 +1,59 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import NotFound from "./components/NotFound";
+import Cart from "./components/Cart";
+
+import "react-toastify/dist/ReactToastify.css";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { loadUser } from "./slices/authSlice";
+import CheckoutSuccess from "./components/CheckoutSuccess";
+import Dashboard from "./components/admin/Dashboard";
+import Products from "./components/admin/Products";
+import Users from "./components/admin/Users";
+import Orders from "./components/admin/Oders";
+import Summary from "./components/admin/Summary";
+import CreateProduct from "./components/admin/CreateProduct";
+
+function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadUser(null));
+  }, [dispatch]);
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <ToastContainer />
+        <NavBar />
+        <div className="content-container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout-success" element={<CheckoutSuccess />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/admin" element={<Dashboard />}>
+              <Route path="summary" element={<Summary />} />
+              <Route path="products" element={<Products />}>
+                <Route path="create-product" element={<CreateProduct />} />
+              </Route>
+              <Route path="users" element={<Users />} />
+              <Route path="orders" element={<Orders />} />
+            </Route>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/slices/productsSlice.js
@@ -0,0 +1,72 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { url, setHeaders } from "./api";
+import { toast } from "react-toastify";
+
+const initialState = {
+  items: [],
+  status: null,
+  createStatus: null,
+};
+
+export const productsFetch = createAsyncThunk(
+  "products/productsFetch",
+  async () => {
+    try {
+      const response = await axios.get(`${url}/products`);
+
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
+export const productsCreate = createAsyncThunk(
+  "products/productsCreate",
+  async (values) => {
+    try {
+      const response = await axios.post(
+        `${url}/products`,
+        values,
+        setHeaders()
+      );
+
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response?.data);
+    }
+  }
+);
+
+const productsSlice = createSlice({
+  name: "products",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [productsFetch.pending]: (state, action) => {
+      state.status = "pending";
+    },
+    [productsFetch.fulfilled]: (state, action) => {
+      state.items = action.payload;
+      state.status = "success";
+    },
+    [productsFetch.rejected]: (state, action) => {
+      state.status = "rejected";
+    },
+    [productsCreate.pending]: (state, action) => {
+      state.createStatus = "pending";
+    },
+    [productsCreate.fulfilled]: (state, action) => {
+      state.items.push(action.payload);
+      state.createStatus = "success";
+      toast.success("Product Created!");
+    },
+    [productsCreate.rejected]: (state, action) => {
+      state.createStatus = "rejected";
+    },
+  },
+});
+
+export default productsSlice.reducer;
